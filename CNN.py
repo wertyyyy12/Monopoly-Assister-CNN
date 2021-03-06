fb399bba7de6e122fabe529cedf8f8fd3b557182
Akilan Babu <wertyyyy102@gmail.com>
2021-03-05 18:31:50 -0800

    added comments, clean up

--- CNN.py
+++ CNN.py
@@ -1,13 +1,65 @@
 import cv2
 from icecream import ic
 import numpy as np
-from pprint import pprint as p
-# import math
 import glob
 
-# img = cv2.imread('Illinois1.png')
-# def s(array):
-#     print(f'size was {np.shape(array)}')
+
+def slidingWindowIndices(imageSize, kernelSize, depth): #gets the relevant sliding window indices so that a kernel can be easily convolved/cross-correlated
+    kernelSize = kernelSize[0]
+    kernelCellSize = pow(kernelSize, 2)
+    
+    imageHeight = imageSize[0]
+    imageWidth = imageSize[1]
+    imageCellSize = imageHeight * imageWidth
+    indices = np.arange(imageHeight * imageWidth).reshape(imageHeight, imageWidth)
+    indices3D = np.arange(imageHeight * imageWidth * depth)
+    
+
+    numberSlidingWindows = (imageHeight - kernelSize + 1) * (imageWidth - kernelSize + 1)
+
+
+    offsets = np.zeros((kernelSize, kernelSize), dtype=int)
+    i = np.arange(kernelSize)
+    offsets[i, :] = (imageWidth - kernelSize) * i
+
+    offsets = offsets.flatten(order='F')
+    wastedWindows = (imageWidth - kernelSize - 1) * imageHeight
+    invalidImagePortionH = np.delete(indices, np.arange((imageWidth - kernelSize)+1), axis=1).flatten() #remove parts of the image that cant be convoluted
+    invalidImagePortionW = np.delete(indices, np.arange((imageHeight - kernelSize)+1), axis=0).flatten()
+
+    invalidImagePortion = np.concatenate((invalidImagePortionH, invalidImagePortionW))
+    invalidImagePortion = invalidImagePortion[invalidImagePortion < (numberSlidingWindows + wastedWindows)]
+
+    indexer = np.arange(kernelCellSize).reshape(1, -1) + (np.arange(numberSlidingWindows + wastedWindows).reshape(-1, 1)) + offsets
+
+    
+    indexer = np.delete(indexer, invalidImagePortion, axis=0)  
+
+    d = np.arange(depth)
+    newIndexer = np.zeros((np.shape(indexer)[0], (kernelCellSize * depth)))
+
+    
+    newPeek = np.zeros(depth)
+
+    newPeek[d]= d * imageCellSize #add the offsets to peek depth-wise
+
+
+    oldIndexShape = indexer.shape
+    indexerCellSize = oldIndexShape[0] * oldIndexShape[1]
+    
+    
+    indexer = np.broadcast_to(indexer, (depth, indexer.shape[0], indexer.shape[1])) #extend indexer array depth-wise 
+    newPeek = np.broadcast_to(newPeek[..., None], (newPeek.shape[0], indexerCellSize)) #repeat each number of newPeek (indexer.shape[0] * indexer.shape[1]) times (assume indexer isn't flattened yet)
+    newPeek = np.resize(newPeek, (depth, oldIndexShape[0], oldIndexShape[1])) #resize the repeated newPeek array so that each number of the old newPeek is now a matrix of old indexer's shape 
+    
+    
+    newIndexer = indexer + newPeek
+    newIndexer = np.concatenate(newIndexer, axis=1)
+    
+
+
+
+    return indices3D.flatten()[np.array(newIndexer, dtype=int)]
 
 class CNN:
     def __init__(self, dir):
@@ -47,111 +99,16 @@ class CNN:
         lastOutputs = np.array(self.log[-1]['outputs'])
         flattenedOutputsTwo = lastOutputs.reshape(3000, -1)
         ic(np.shape(flattenedOutputsTwo))
-        
 
-    # def backprop(self):
-
-
-
-
-
-
-# first = CNN("data/Training-Set-10/*.png")
-# first.convolve(first.images, np.random.rand(3000, 3, 3, 3))
-# first.flatten()
-
-def slidingWindow(imageSize, kernelSize, depth):
-    kernelSize = kernelSize[0]
-    kernelCellSize = pow(kernelSize, 2)
-    
-    imageHeight = imageSize[0]
-    imageWidth = imageSize[1]
-    imageCellSize = imageHeight * imageWidth
-    indices = np.arange(imageHeight * imageWidth).reshape(imageHeight, imageWidth)
-    indices3D = np.arange(imageHeight * imageWidth * depth).reshape(imageHeight, imageWidth, depth)
-    
-
-    numberSlidingWindows = (imageHeight - kernelSize + 1) * (imageWidth - kernelSize + 1)
-
-
-    offsets = np.zeros((kernelSize, kernelSize), dtype=int)
-    i = np.arange(kernelSize)
-    offsets[i, :] = (imageWidth - kernelSize) * i
-
-    offsets = offsets.flatten(order='F')
-    wastedWindows = (imageWidth - kernelSize - 1) * imageHeight
-    invalidImagePortionH = np.delete(indices, np.arange((imageWidth - kernelSize)+1), axis=1).flatten() #remove parts of the image that cant be convoluted
-    invalidImagePortionW = np.delete(indices, np.arange((imageHeight - kernelSize)+1), axis=0).flatten()
-
-    invalidImagePortion = np.concatenate((invalidImagePortionH, invalidImagePortionW))
-    invalidImagePortion = invalidImagePortion[invalidImagePortion < (numberSlidingWindows + wastedWindows)]
-
-    
-    # ic(numberSlidingWindows + wastedWindows, invalidImagePortionW, invalidImagePortionH)
-
-
-    indexer = np.arange(kernelCellSize).reshape(1, -1) + (np.arange(numberSlidingWindows + wastedWindows).reshape(-1, 1)) + offsets
-
-    
-    indexer = np.delete(indexer, invalidImagePortion, axis=0)  
-    # indexer = indexer[:-(imageHeight - kernelSize + 1), :]
-    # indexer = np.expand_dims(indexer, axis=2)
-    # indexer = np.moveaxis(indexer, 1, 2)
-
-    # ic(np.shape(indexer))
-
-
-
-    i = np.arange(np.shape(indexer)[0])
-
-    d = np.arange(depth)
-    # ic(indexer[i, :])
-    newIndexer = np.zeros((np.shape(indexer)[0], (kernelCellSize * depth)))
-
-    # ic(np.array(newIndexer[:, i]+(d*imageCellSize)).shape)
-    # newIndexer[i, :] = d*imageCellSize 
-    
-    newPeek = np.zeros(depth)
-
-    #add the offsets to peek depth-wise
-    newPeek[d] = d * imageCellSize
-
-
-    oldIndexShape = indexer.shape
-    indexerCellSize = oldIndexShape[0] * oldIndexShape[1]
-    ic(indexer.shape)
-    
-    
-    # ic(indexer)
-    indexer = np.broadcast_to(indexer, (depth, indexer.shape[0], indexer.shape[1]))
-    newPeek = np.broadcast_to(newPeek[..., None], (newPeek.shape[0], indexerCellSize))
-    newPeek = np.resize(newPeek, (depth, oldIndexShape[0], oldIndexShape[1]))
-    
-    
-    newIndexer = indexer + newPeek
-    newIndexer = np.concatenate(newIndexer, axis=1)
-    
-
-
-
-    return indices3D.flatten()[np.array(newIndexer, dtype=int)]
 
 
 
 a = np.reshape(np.arange(36*3), (6, 6, 3))
 b = np.reshape(np.arange(9), (3, 3))
 
-ic(slidingWindow((6, 6), (3, 4), 3))
-
-
-
-
-
-
-
+ic(slidingWindowIndices((6, 6), (3, 4), 3))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-# ic(split)
 
 
