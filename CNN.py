48002df018a22889aa6aa5e68893279fb995aaa9
Akilan Babu <wertyyyy102@gmail.com>
2021-03-21 21:44:49 -0700

    basic "blueprint" to create custom configurations

--- CNN.py
+++ CNN.py
@@ -1,9 +1,12 @@
+from typing import final
 import cv2
 from icecream import ic
 import numpy as np
 import glob
 #-- timer for timing things yay --
 import time
+
+
 
 class TimerError(Exception):
     """A custom exception used to report errors in use of Timer class"""
@@ -26,55 +29,76 @@ class Timer:
 
         elapsed_time = time.perf_counter() - self._start_time
         self._start_time = None
-        print(f"Elapsed time: {elapsed_time:0.6f} seconds")
+        return '{:.6f}'.format(elapsed_time)
+        #print(f"Elapsed time: {elapsed_time:0.6f} seconds")
 # -- -- 
 
 
 t = Timer()
 
 class CNN:
-    def __init__(self, dir):
+    def __init__(self, dir, blueprint):
         self.log = []
         self.dir = dir
+        self.blueprint = blueprint
         self.finalWeights = None
         filenames = glob.glob(dir) #get all filenames in the specfied directory
         filenames.sort() #sort by alpha order
-        self.images = np.array([cv2.imread(img) for img in filenames]) #read each file
+        self.images = np.array([cv2.imread(img) for img in filenames]) / 255 #read each file, constrain to interval (0,1)
         ic(np.shape(self.images))
+        
 
         print(f'Loaded {len(filenames)} images from "{dir}"')
 
+        #read the blueprint given and feedforward with it
+        currentImageSet = self.images #initialize the first image set to work with as the images given
+        for operation in blueprint:
+            operationType = operation['operationType']
+            if operationType == 'convolve':
+                kernel = operation['kernel']
+                t.start()
+                bias = operation['bias']
+                operation['outputImageSet'] = self.convolve(kernel, bias, currentImageSet)
+                currentImageSet = operation['outputImageSet']
+                print(f'Completed convolution in {t.stop()} seconds, image ID {np.sum(currentImageSet / 10000)}')
+            if operationType == 'ReLU':
+                t.start()
+                operation['outputImageSet'] = np.maximum(0, currentImageSet)
+                ic(operation['outputImageSet'].shape)
+                currentImageSet = operation['outputImageSet']
+                print(f'Completed ReLU in {t.stop()} seconds, image ID {np.sum(currentImageSet / 10000)}')
+        ic(self.blueprint)
+                
+
     def convolve(self, kernel, bias, images, train=False):
-        kernelShape = (3,3)
+        kernelShape = (kernel.shape[0], kernel.shape[1])
         
+        #ensure that the views don't look anywhere except for up and down on the images with axis=(1,2)
         views = np.lib.stride_tricks.sliding_window_view(images, kernelShape, axis=(1, 2))
-        kernel = kernel[None, None, None, ...] #modify kernel shape for broadcasting
+        kernel = kernel[np.newaxis, np.newaxis, np.newaxis, ...] #modify kernel shape for broadcasting
 
         #element wise multiply the views with the kernels, then sum over a LOT of axes to get the final result
         #einsum is so smart it even broadcasts the element wise multiplication O_O
         finalConvolution = np.einsum('ijklmn,ijklmn->ijk', views, kernel) 
-  
+        finalConvolution += bias 
+
         ic(finalConvolution.shape)
-        
-        
 
-        
-        if not train: 
-            self.log.append({
-                'type': 'convolution',
-                'kernel': kernel,
-                'inputs': images, #contains all of the images that were convoluted 
-                #'outputs': convolved #contains all of the now convoluted images
-            })
-        
-
-        
-    def ReLU(self, matrix):
-        return np.maximum(0, matrix)
+        return finalConvolution
 
 
-first = CNN('data/Training-Set-10/*.png')
-first.convolve(np.arange(27).reshape(3, 3, 3), 10, first.images)
+
+rng = np.random.default_rng()
+first = CNN('data/Training-Set-10/*.png', [{
+    'operationType': 'convolve', 
+    'kernel': (np.random.random_sample((3, 3, 3)) - 0.5), 
+    'bias': 0.05,
+    'outputImageSet': None
+}, 
+{
+    'operationType': 'ReLU',
+    'outputImageSet': None
+}])
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
